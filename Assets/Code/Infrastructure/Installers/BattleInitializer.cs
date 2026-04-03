82a45da3126f688388e34427791f21a7fa0f967b
Tomas Lysionok <tomasl1996@gmail.com>
2026-04-04 01:51:04 +0300

    BattleInitializer now handles EnemySpawn start/stop.

--- Assets/Code/Infrastructure/Installers/BattleInitializer.cs
+++ Assets/Code/Infrastructure/Installers/BattleInitializer.cs
@@ -1,16 +1,21 @@
+using Code.Gameplay.Battle;
 using Code.Gameplay.Cameras.Services;
+using Code.Gameplay.Characters.Enemies.Services;
 using Code.Gameplay.Characters.Heroes.Services;
+using Code.Gameplay.Lifetime.Behaviours;
 using Code.Infrastructure.Instantiation;
 using Code.Infrastructure.UIManagement;
 using Code.UI;
+using System;
 using UnityEngine;
 using Zenject;
 
 namespace Code.Infrastructure.Installers
 {
-	public class BattleInitializer : MonoBehaviour, IInitializable
-	{
-		[SerializeField] private Camera _mainCamera;
+	public class BattleInitializer : MonoBehaviour, IInitializable, IDisposable
+    {
+        [SerializeField] private BattleId _battleId;
+        [SerializeField] private Camera _mainCamera;
 		[SerializeField] private Transform _uiRoot;
 		
 		private ICameraProvider _cameraProvider;
@@ -19,16 +24,20 @@ namespace Code.Infrastructure.Installers
 		private IInstantiateService _instantiateService;
 		private IUIRootProvider _uiRootProvider;
 		private IUiService _uiService;
+		private IEnemySpawner _enemySpawner;
+        private IHeroProvider _heroProvider;
 
-		[Inject]
+        [Inject]
 		private void Construct(
 			ICameraProvider cameraProvider,
 			IHeroFactory heroFactory,
 			IInstantiateService instantiateService,
 			IInstantiator instantiator,
 			IUIRootProvider uiRootProvider,
-			IUiService uiService
-		)
+			IUiService uiService,
+			IEnemySpawner enemySpawner,
+            IHeroProvider heroProvider
+        )
 		{
 			_uiService = uiService;
 			_uiRootProvider = uiRootProvider;
@@ -36,7 +45,9 @@ namespace Code.Infrastructure.Installers
 			_instantiator = instantiator;
 			_heroFactory = heroFactory;
 			_cameraProvider = cameraProvider;
-		}
+            _enemySpawner = enemySpawner;
+            _heroProvider = heroProvider;
+        }
     
 		public void Initialize()
 		{
@@ -45,8 +56,30 @@ namespace Code.Infrastructure.Installers
 			_uiRootProvider.SetUiRoot(_uiRoot);
 			
 			_heroFactory.CreateHero(Vector3.zero, Quaternion.identity);
+            _enemySpawner.StartSpawning(_battleId);
 
-			_uiService.OpenWindow<HudWindow>();
+			SubscribeToOnHeroDeath();
+
+            _uiService.OpenWindow<HudWindow>();
 		}
-	}
+
+        public void Dispose()
+        {
+            if (_heroProvider?.Health != null)
+                _heroProvider.Health.OnDeath -= _enemySpawner.StopSpawning;
+        }
+
+		private void SubscribeToOnHeroDeath()
+		{
+            if (_heroProvider.Health != null)
+			{
+                _heroProvider.Health.OnDeath += _enemySpawner.StopSpawning;
+            }
+			else
+			{
+                throw new MissingComponentException(
+					$"[BattleInitializer] The Hero was created, but no {nameof(Health)} component was found.");
+            }
+        }
+    }
 }
